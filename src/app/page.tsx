8259ce24c582178466a3bae14ada4082f134b437
Anh Cao <anh.cao@perchinsights.com>
2024-03-27 16:03:04 -0400

    chore: :lipstick: compact view of container with to 65% on lg screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import OneTimePaymentFeat from "./features/OneTimePayment";
 
 export default function Home() {
   return (
-    <main className="container h-screen mx-auto p-4">
+    <main className="container lg:w-[65%] h-screen mx-auto p-4">
       <OneTimePaymentFeat />
     </main>
   );
